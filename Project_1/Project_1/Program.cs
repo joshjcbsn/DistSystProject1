5ae03dc052f104484fc67b913ae90bde59cf6982
joshjcbsn <joshjcbsn@gmail.com>
2016-10-20 00:44:08 -0400

    remove print

--- Project_1/Project_1/Program.cs
+++ Project_1/Project_1/Program.cs
@@ -39,7 +39,7 @@ namespace Project_1
                 }
             }
             Node process = new Node(N, tcpConfig[N]);
-            Task listener = Task.Factory.StartNew(() => process.getConnections());
+          
             using (StreamReader treeReader = new StreamReader("tree.txt"))
             {
                 string line;
@@ -57,7 +57,7 @@ namespace Project_1
                         process.addNeighbor(parent, tcpConfig[parent]);
                 }
             }
-            
+            Task listener = Task.Factory.StartNew(() => process.getConnections());
 
             while (true)
             {
